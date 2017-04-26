89d40244f95ef321adaf6004139f90e85c6d46fe
smita kumari <kumarismita200@gmail.com>
2017-04-26 19:30:30 +0530

    updatefiee

--- TodoFolder/Todo24apupdated/ToDo/app/src/main/java/com/app/todo/login/interactor/LoginInteractorInterface.java
+++
@@ -1,8 +0,0 @@
-package com.app.todo.login.interactor;
-
-
-
-public interface LoginInteractorInterface {
-    void getLoginResponse(String email_login,String password_login);
-
-}
--- TodoFolder/Todo24apupdated/ToDo/app/src/main/java/com/app/todo/todohome/ui/CallBackItemInterface.java
+++
@@ -1,8 +0,0 @@
-package com.app.todo.todohome.ui;
-
-import com.app.todo.todohome.model.TodoHomeDataModel;
-
-
-public interface CallBackItemInterface {
-    public void onItemClick(TodoHomeDataModel dataModel);
-}
