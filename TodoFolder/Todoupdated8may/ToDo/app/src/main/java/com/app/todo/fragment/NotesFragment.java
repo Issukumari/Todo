8e637e005c736ec39114dc681febcf6118f43d02
smita kumari <kumarismita200@gmail.com>
2017-05-08 19:35:58 +0530

    updated file

---
+++ TodoFolder/Todoupdated8may/ToDo/app/src/main/java/com/app/todo/fragment/NotesFragment.java
@@ -0,0 +1,35 @@
+package com.app.todo.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.app.todo.R;
+
+import com.app.todo.todohome.model.TodoHomeDataModel;
+import com.app.todo.todohome.ui.TodoHomeActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotesFragment extends Fragment
+{
+    public static final String TAG = "NotesFragment";
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.recycler_view, container, false);
+
+        return view;
+    }
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        ((TodoHomeActivity)getActivity()).setTitle("Notes");
+    }
+}
---
+++ TodoFolder/Todoupdated8may/ToDo/app/src/main/java/com/app/todo/todohome/model/TodoHomeDataModel.java
@@ -0,0 +1,68 @@
+package com.app.todo.todohome.model;
+
+public class TodoHomeDataModel {
+    public boolean isArchieve;
+    private int id;
+    private String title;
+    private String description;
+    private String reminderDate;
+
+    private String startdate;
+
+    public TodoHomeDataModel() {
+
+
+    }
+
+    public String getStartdate() {
+        return startdate;
+    }
+
+    public void setStartdate(String startdate) {
+        this.startdate = startdate;
+    }
+
+    public boolean isArchieve() {
+        return isArchieve;
+    }
+
+    public void setArchieve(boolean archieve) {
+        isArchieve = archieve;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public String getReminderDate() {
+        return reminderDate;
+    }
+
+    public void setReminderDate(String reminderDate) {
+        this.reminderDate = reminderDate;
+    }
+
+
+
+}
