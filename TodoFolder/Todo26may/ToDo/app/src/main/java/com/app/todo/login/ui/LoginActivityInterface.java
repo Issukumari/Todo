7c74aec7c954078d823c3380fdc931074c139e9d
smita kumari <kumarismita200@gmail.com>
2017-05-26 19:30:31 +0530

    updated file

---
+++ TodoFolder/Todo26may/ToDo/app/src/main/java/com/app/todo/login/ui/LoginActivityInterface.java
@@ -0,0 +1,23 @@
+package com.app.todo.login.ui;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.app.todo.registration.model.UserDatamodel;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by bridgeit on 21/4/17.
+ */
+public interface LoginActivityInterface extends View.OnClickListener {
+    void loginSuccess(UserDatamodel Model, String uid);
+    void loginFailure(String message);
+    void showProgressDialog(String mesage);
+    void hideProgressDialog();
+    void fbFailure(String message);
+    void googlesigninfailure(String message);
+    void showError(int errorType);
+    void getfacebookdata(String uid, JSONObject jsonObject) throws JSONException;
+}
